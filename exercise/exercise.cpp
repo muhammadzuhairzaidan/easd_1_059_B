f8fb214905eb51884791279346d7e7c45c8d13d3
Muhammad Zuhair Zaidan <muhammad.zuhair.ft22@mail.umy.ac.id>
2023-04-07 11:33:10 +0700

    mendeklarasikan mid

--- exercise/exercise.cpp
+++ exercise/exercise.cpp
@@ -47,6 +47,12 @@ void mergeSort(int low, int high) {
     if (low >= high) {
         return;
     }
+    int mid = (low + high) / 2;
+    mergeSort(low, mid);
+    mergeSort(mid + 1, high);
+    merge(low, mid, high);
+}
 
+v
    
 
